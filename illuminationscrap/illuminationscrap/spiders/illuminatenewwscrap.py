9ded87d47a260ee4e09fb46ba1d7d43c16a0fdcb
oblivion4us <nft4oblivion@gmail.com>
2024-09-21 18:52:05 +0200

    requests for luminaires

--- illuminationscrap/illuminationscrap/spiders/illuminatenewwscrap.py
+++ illuminationscrap/illuminationscrap/spiders/illuminatenewwscrap.py
@@ -7,4 +7,10 @@ class IlluminatenewwscrapSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/kaliningrad/category/svet"]
 
     def parse(self, response):
-        pass
+        illuminaires = response.css('div.WdR1o')
+        for illuminaire in illuminaires:
+            yield {
+                'name' : illuminaire.css("span.itemprop = 'name'").get(),
+                'price' : illuminaire.css('span.ui-LD-ZU KIkOH').get(),
+                'link' : illuminaire.css('a').attrib['href']
+            }
